bc2759393e3453bbc3cc4e4ad7f6709dc16aac50
eran505 <eran505@gmail.com>
2017-12-24 18:56:37 +0200

    bla

--- topics_extraction_with_nmf_lda.py
+++ topics_extraction_with_nmf_lda.py
@@ -2,37 +2,39 @@
 =======================================================================================
 Topic extraction with Non-negative Matrix Factorization and Latent Dirichlet Allocation
 =======================================================================================
-
-This is an example of applying Non-negative Matrix Factorization
-and Latent Dirichlet Allocation on a corpus of documents and
-extract additive models of the topic structure of the corpus.
-The output is a list of topics, each represented as a list of terms
-(weights are not shown).
-
-The default parameters (n_samples / n_features / n_topics) should make
-the example runnable in a couple of tens of seconds. You can try to
-increase the dimensions of the problem, but be aware that the time
-complexity is polynomial in NMF. In LDA, the time complexity is
-proportional to (n_samples * iterations).
 """
 
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-#         Lars Buitinck
-#         Chyi-Kwei Yau <chyikwei.yau@gmail.com>
-# License: BSD 3 clause
+import string
+import re
 
-from __future__ import print_function
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+import pre_process as pp
+import csv
 from time import time
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.decomposition import NMF, LatentDirichletAllocation
+from sklearn.decomposition import  LatentDirichletAllocation
 from sklearn.datasets import fetch_20newsgroups
-
+import pandas as pd
 n_samples = 2000
 n_features = 1000
 n_topics = 10
 n_top_words = 20
 
+from nltk.corpus import stopwords
+
+cachedStopWords = stopwords.words("english")
+
+
+def str_to_int(myString):
+    if len(myString) < 1 :
+        return -1
+    #pos = myString.find('.')
+    #new_string = myString[pos-1:pos+2]
+    int_num = int(float(myString))
+    return int_num
 
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
@@ -49,10 +51,50 @@ def print_top_words(model, feature_names, n_top_words):
 
 print("Loading dataset...")
 t0 = time()
+#########reading data-set using csv ########################
+d=[]
+with open('/home/ise/Downloads/proc_17_108_unique_comments_text_dupe_count.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    c=0
+    for row in reader:
+        d.append({'id':row[0],'text_data':row[1],'dupe_count':row[2] })
+
+df= pd.DataFrame(d,columns=['id','text_data','dupe_count'])
+df = df.iloc[2:]
+print "list=",list(df)
+print df.shape
+print "#"*100
+df['dupe_count_int']=df['dupe_count'].apply(lambda x: str_to_int(x))
+good_bye_list = ['dupe_count']
+df.drop(good_bye_list, axis=1, inplace=True)
+df= df.loc[df['dupe_count_int'] >= 0]
+print df.shape
+print ( "done in %0.3fs." % (time() - t0) )
+
+
+##############################################################
+"""
+==========================================================
+Text Pre-processing with NLTK
+==========================================================
+"""
+
+
+print("data cleaning...")
+t0 = time()
+df['text_data']=df['text_data'].apply(lambda x: pp.patent_to_words(x))
+print ( "done in %0.3fs." % (time() - t0) )
+reltiv_path = "/home/ise/NLP/data_sets"
+df.to_csv(reltiv_path+"/res.csv",sep=',')
+exit()
+###############################################################
+
+
+
+
 dataset = fetch_20newsgroups(shuffle=True, random_state=1,
                              remove=('headers', 'footers', 'quotes'))
 data_samples = dataset.data[:n_samples]
-print("done in %0.3fs." % (time() - t0))
 
 # Use tf-idf features for NMF.
 print("Extracting tf-idf features for NMF...")
@@ -72,23 +114,12 @@ t0 = time()
 tf = tf_vectorizer.fit_transform(data_samples)
 print("done in %0.3fs." % (time() - t0))
 
-# Fit the NMF model
-print("Fitting the NMF model with tf-idf features, "
-      "n_samples=%d and n_features=%d..."
-      % (n_samples, n_features))
-t0 = time()
-nmf = NMF(n_components=n_topics, random_state=1,
-          alpha=.1, l1_ratio=.5).fit(tfidf)
-print("done in %0.3fs." % (time() - t0))
 
-print("\nTopics in NMF model:")
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-print_top_words(nmf, tfidf_feature_names, n_top_words)
 
 print("Fitting LDA models with tf features, "
       "n_samples=%d and n_features=%d..."
       % (n_samples, n_features))
-lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
+lda = LatentDirichletAllocation(n_components=n_topics, max_iter=5,
                                 learning_method='online',
                                 learning_offset=50.,
                                 random_state=0)
